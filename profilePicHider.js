e4d6f3e42b688b6c30e16a0a964ab30406c10daf
Rob Seaver <rob.seaver@gmail.com>
2020-09-20 15:31:53 -0500

    feat: Add additional elements for Facebook

--- profilePicHider.js
+++ profilePicHider.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Hide Social Media Profile Pictures
 // @namespace     http://tampermonkey.net/
-// @version       1.0.0
+// @version       1.0.1
 // @description   Hide all social media profile pictures until I can figure out a way to hide just the ones with face masks
 // @author        Rob Seaver <rob.seaver@gmail.com>
 // @include       https://www.facebook.com/*
@@ -10,14 +10,26 @@
 // @grant         none
 // ==/UserScript==
 
-const selectors = [
-  'img._s0._4ooo._5xib._5sq7._44ma._rw.img',
-  'img._3me-._3mf1.img',
+const instagramSelectors = ['img._6q-tv'];
+const twitterSelectors = [
   '.css-1dbjc4n.r-18kxxzh.r-1wbh5a2.r-13qz1uu',
-  'img._6q-tv',
   '.css-1dbjc4n.r-sdzlij.r-1p0dtai.r-1mlwlqe.r-1d2f490.r-1udh08x.r-u8s1d.r-zchlnj.r-ipm5af.r-417010'
 ];
+const facebookSelectors = [
+  'img._s0._4ooo._5xib._5sq7._44ma._rw.img',
+  'img._3me-._3mf1.img',
+  'img._62bh.img _8o._8r._2qgu.img',
+  'div._ohe.lfloat'
+];
 
+const selectors = [
+  [...instagramSelectors],
+  [...twitterSelectors],
+  [...facebookSelectors]
+];
+
+// I don't like this as a catch-all. A better Javascript
+// developer should be able to think about this differently
 const events = [
   'load',
   'loading',
@@ -28,24 +40,23 @@ const events = [
 ];
 
 ((selectors) => {
-  events.forEach((state) => {
-    document.addEventListener(state, () => {
+  events.forEach((e) => {
+    document.addEventListener(e, () => {
       hideProfilePictures(selectors);
     });
   });
 })(selectors.join(','));
 
 const hideProfilePictures = (selectors) => {
-  const profilePics = document.querySelectorAll(selectors);
-  if (!!profilePics.length) {
-    try {
-      profilePics.forEach((pic) => {
-        pic.remove();
-      });
-      console.info(`Hid ${profilePics.length} profile pictures`);
-    } catch (error) {
-      console.warn('Error hiding pictures', error);
-      hideProfilePictures(selectors);
-    }
+  const elements = document.querySelectorAll(selectors);
+  if (elements.length === 0) {
+    return;
+  }
+  try {
+    elements.forEach((e) => {
+      e.remove();
+    });
+  } catch (error) {
+    hideProfilePictures(selectors);
   }
 };
