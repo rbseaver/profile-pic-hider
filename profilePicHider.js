39387ae7c2f3b9acc5ff47d7c809e16d847f4c51
Rob Seaver <rob.seaver@gmail.com>
2020-09-20 22:30:16 -0500

    fix: Tweak css references for Facebook and Twitter

--- profilePicHider.js
+++ profilePicHider.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Hide Social Media Profile Pictures
 // @namespace     http://tampermonkey.net/
-// @version       1.0.1
+// @version       1.0.3
 // @description   Hide all social media profile pictures until I can figure out a way to hide just the ones with face masks
 // @author        Rob Seaver <rob.seaver@gmail.com>
 // @include       https://www.facebook.com/*
@@ -10,24 +10,23 @@
 // @grant         none
 // ==/UserScript==
 
+// These can be improved, I'm certain.
 const instagramSelectors = ['img._6q-tv'];
 const twitterSelectors = [
-  '.css-1dbjc4n.r-18kxxzh.r-1wbh5a2.r-13qz1uu',
-  '.css-1dbjc4n.r-sdzlij.r-1p0dtai.r-1mlwlqe.r-1d2f490.r-1udh08x.r-u8s1d.r-zchlnj.r-ipm5af.r-417010'
+  'div.css-1dbjc4n.r-18kxxzh.r-1wbh5a2.r-13qz1uu',
+  'div.css-1dbjc4n.r-sdzlij.r-1p0dtai.r-1mlwlqe.r-1d2f490.r-1udh08x.r-u8s1d.r-zchlnj.r-ipm5af.r-417010',
+  'div.css-1dbjc4n.r-1mlwlqe.r-z80fyv.r-1udh08x.r-19wmn03.r-417010',
+  'div.css-1dbjc4n.r-19z6qd4.r-sdzlij.r-rs99b7.r-1p0dtai.r-1mi75qu.r-1d2f490.r-1ny4l3l.r-u8s1d.r-zchlnj.r-ipm5af'
 ];
 const facebookSelectors = [
-  'img._s0._4ooo._5xib._5sq7._44ma._rw.img',
-  'img._3me-._3mf1.img',
-  'img._62bh.img _8o._8r._2qgu.img',
-  'div._ohe.lfloat',
-  'span._3eoh'
+  'img._rw.img',
+  'img._3me-',
+  'img._6tb5.img',
+  'img._5bow.img',
+  'img._62bh.img'
 ];
 
-const selectors = [
-  [...instagramSelectors],
-  [...twitterSelectors],
-  [...facebookSelectors]
-];
+const selectors = [...instagramSelectors, ...twitterSelectors, ...facebookSelectors];
 
 // I don't like this as a catch-all. A better Javascript
 // developer should be able to think about this differently
@@ -36,7 +35,7 @@ const events = [
   'loading',
   'readystatechange',
   'scroll',
-  'mouseenter',
+  'mouseover',
   'DOMContentLoaded'
 ];
 
