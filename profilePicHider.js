0af09bc50d6be18bf668f07a8dc9ef69d162e0df
Rob Seaver <rob.seaver@gmail.com>
2020-12-09 21:31:34 -0600

    fix(facebook): hide profile pics in video chat area

--- profilePicHider.js
+++ profilePicHider.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Hide Social Media Profile Pictures
 // @namespace     http://tampermonkey.net/
-// @version       1.2.2
+// @version       1.2.3
 // @description   Hide all social media profile pictures until I can figure out a way to hide just the ones with face masks
 // @author        Rob Seaver <rob.seaver@gmail.com>
 // @updateURL     https://raw.githubusercontent.com/rbseaver/tampermonkey-scripts/master/profilePicHider.js
